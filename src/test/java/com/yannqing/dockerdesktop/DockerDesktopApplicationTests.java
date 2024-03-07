9f63182767fb4538893aca5ef667973b7739eb2c
yannqing <67121851@qq.com>
2024-03-07 17:07:40 +0800

    init

--- src/test/java/com/yannqing/dockerdesktop/DockerDesktopApplicationTests.java
+++ src/test/java/com/yannqing/dockerdesktop/DockerDesktopApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DockerDesktopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
